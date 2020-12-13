aec15ab9cf909c76cc8bbff14cde7d20f2136c1f
Enrique Arrabal <enrike9066@gmail.com>
2020-12-13 21:24:23 +0100

    String Multiply kata done
    refactor needed

--- katas/stringMultiply/stringMultiply.go
+++ katas/stringMultiply/stringMultiply.go
@@ -1,6 +1,45 @@
 package main
 
+import (
+	"math"
+	"strings"
+)
 
-func main(){
-	println("hello world")
+func main() {
+	println(stringMultiply("14", "123"))
+}
+
+func stringMultiply(multiplier string, multiplicand string) (result string) {
+	if(multiplier[0] == '0' || multiplicand[0]== '0'){
+		result = "0"
+		return
+	}
+	var product []int
+	product = make([]int, len(multiplier)+len(multiplicand))
+	for multiplierPosition := len(multiplier) - 1; multiplierPosition >= 0; multiplierPosition-- {
+		for multiplicandPosition := len(multiplicand) - 1; multiplicandPosition >= 0; multiplicandPosition-- {
+			positionResult := product[multiplierPosition+multiplicandPosition+1] + runeMultiplier(multiplier[multiplierPosition], multiplicand[multiplicandPosition])
+			product[multiplierPosition+multiplicandPosition+1] = int(math.Mod(float64(positionResult), 10))
+			product[multiplierPosition+multiplicandPosition] += positionResult / 10
+		}
+	}
+	result = stringBuilder(product)
+	if( result[0] == '0'){
+		result = result[1:]
+	}
+	return
+}
+
+func runeMultiplier(x byte, y byte) (result int) {
+	result = int(x-'0') * int(y-'0')
+	return
+}
+
+func stringBuilder(array []int) (result string) {
+	var stringResult strings.Builder
+	for _, value := range array {
+		stringResult.WriteRune(rune(value + 48))
+	}
+	result = stringResult.String()
+	return
 }
--- katas/stringMultiply/stringMultiply_test.go
+++ katas/stringMultiply/stringMultiply_test.go
@@ -1,10 +1,22 @@
 package main
 
-import(
+import (
 	"testing"
-	"github.com/stretchr/testify/assert"
-) 
 
-func TestStringMultipy( testing *testing.T){
-	assert.Equal(testing,1,1,"should be equal")
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStringMultipy(testing *testing.T) {
+	var valuesToTest = []struct {
+		x, y, result string
+	}{
+		{"12341234", "1234", "15229082756"},
+		{"1234", "12341234", "15229082756"},
+		{"0", "12341234", "0"},
+		{"123123", "0", "0"},
+	}
+	for i := 0; i < len(valuesToTest); i++ {
+		assert.Equal(testing, valuesToTest[i].result, stringMultiply(valuesToTest[i].x, valuesToTest[i].y), "should be equal")
+	}
+
 }
