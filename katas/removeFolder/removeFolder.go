90b30faa32a74e3d21c51233d4bf749dce610081
Enrique Arrabal <enrike9066@gmail.com>
2020-12-14 22:55:51 +0100

    kata implemented, pass all test
    next step will be refactor

--- katas/removeFolder/removeFolder.go
+++ katas/removeFolder/removeFolder.go
@@ -1,12 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	println(removeFolder([]string{"/a", "/a/b", "/c/d", "/c/d/e", "/c/f", "/c/f/g"}, "c"))
+	fmt.Printf("%v \n", removeFolder([]string{"/a", "/a/b", "/c/d", "/c/d/e", "/c/f", "/c/f/g"}, "c"))
 }
 
-func removeFolder(folderStructure []string, folderName string) (result string) {
+func removeFolder(folderStructure []string, folderName string) (result []string) {
+	var newFolderStructure []string
 	for _, element := range folderStructure {
-		println(element)
+		filteredElement := seekAndDestroy(element, folderName)
+		if len(filteredElement) > 0 {
+			newFolderStructure = append(newFolderStructure,filteredElement)
+		}
 	}
+	result = newFolderStructure
 	return
+}
+
+//🤘
+func seekAndDestroy(element string, folderName string) string {
+	firstOccurrenceIndex := strings.Index(element, folderName)
+	if firstOccurrenceIndex > 0 {
+		return element[:firstOccurrenceIndex-1]
+	} else {
+		return element
+	}
 }
